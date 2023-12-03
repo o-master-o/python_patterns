0d5a177c3577250012f3c86e74e9eeb6b3e8f9a9
o-master-o <kasian.m.w@gmail.com>
2023-12-04 00:53:34 +0100

    add thread save singleton class

---
+++ singletone.py
@@ -0,0 +1,21 @@
+from threading import Lock
+
+
+class SimpleSingleton:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+
+class ThreadSingleton:
+    _instance = None
+    _lock = Lock()
+
+    def __new__(cls, *args, **kwargs):
+        with cls._lock:
+            if cls._instance is None:
+                cls._instance = super().__new__(cls)
+            return cls._instance
--- test/test_singleton_pattern.py
+++ test/test_singleton_pattern.py
@@ -1,5 +1,23 @@
-from singletone import SimpleSingleton
+from threading import Thread
+
+from singletone import SimpleSingleton, ThreadSingleton
+
+
+def get_singleton_obj(id_storage):
+    _id = id(ThreadSingleton())
+    id_storage.append(_id)
 
 
 def test_new_method_singleton_instances_should_have_same_id():
     assert id(SimpleSingleton()) == id(SimpleSingleton())
+
+
+def test_init_method_singleton_instances_should_have_same_id():
+    id_storage = []
+    thread1 = Thread(target=get_singleton_obj, args=(id_storage, ))
+    thread2 = Thread(target=get_singleton_obj, args=(id_storage, ))
+    thread1.start()
+    thread2.start()
+    thread1.join()
+    thread2.join()
+    assert id_storage[0] == id_storage[1]
