9b4db26fc2f375e6675ec1422908a2d98d93f31d
o-master-o <kasian.m.w@gmail.com>
2023-12-04 01:25:10 +0100

    add possibility to create multiple independent singleton subclasses

--- singletone.py
+++ singletone.py
@@ -2,20 +2,20 @@ from threading import Lock
 
 
 class SimpleSingleton:
-    _instance = None
+    _instances = {}
 
     def __new__(cls, *args, **kwargs):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-        return cls._instance
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__new__(cls)
+        return cls._instances[cls]
 
 
 class ThreadSingleton:
-    _instance = None
+    _instances = {}
     _lock = Lock()
 
     def __new__(cls, *args, **kwargs):
         with cls._lock:
-            if cls._instance is None:
-                cls._instance = super().__new__(cls)
-            return cls._instance
+            if cls not in cls._instances:
+                cls._instances[cls] = super().__new__(cls)
+            return cls._instances[cls]
--- test/test_singleton_pattern.py
+++ test/test_singleton_pattern.py
@@ -3,16 +3,31 @@ from threading import Thread
 from singletone import SimpleSingleton, ThreadSingleton
 
 
+class SingletonA(SimpleSingleton):
+    pass
+
+
+class SingletonB(SimpleSingleton):
+    pass
+
+
 def get_singleton_obj(id_storage):
-    _id = id(ThreadSingleton())
+    _id = id(SingletonA())
     id_storage.append(_id)
 
 
-def test_new_method_singleton_instances_should_have_same_id():
-    assert id(SimpleSingleton()) == id(SimpleSingleton())
+def test_for_simple_singleton_same_sub_classes_instances_should_be_the_same_instances_and_different_sub_classes_instances_should_be_the_different():
+    singleton_a1 = SingletonA()
+    singleton_a2 = SingletonA()
+    singleton_b1 = SingletonB()
+    singleton_b2 = SingletonB()
+
+    assert singleton_a1 is singleton_a2
+    assert singleton_b1 is singleton_b2
+    assert singleton_a1 is not singleton_b1
 
 
-def test_init_method_singleton_instances_should_have_same_id():
+def test_for_thread_singleton_same_sub_classes_instances_should_be_the_same_instances_and_different_sub_classes_instances_should_be_the_different():
     id_storage = []
     thread1 = Thread(target=get_singleton_obj, args=(id_storage, ))
     thread2 = Thread(target=get_singleton_obj, args=(id_storage, ))
